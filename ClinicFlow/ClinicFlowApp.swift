bec1c2d124ab05e494f3229d535fd4b372acd542
Thirandidesilva <thirandidesilva2002@gmail.com>
2026-02-20 17:17:29 +0530

    Initial Commit

---
+++ ClinicFlow/ClinicFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlowApp.swift
+//  ClinicFlow
+//
+//  Created by Thirandi De Silva on 2026-02-20.
+//
+
+import SwiftUI
+
+@main
+struct ClinicFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
