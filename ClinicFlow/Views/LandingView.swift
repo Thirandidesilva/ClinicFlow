ba976667db9a22e32810a3e5884920f2a7370e8d
Thirandidesilva <thirandidesilva2002@gmail.com>
2026-02-24 15:07:15 +0530

    Modify the Landing page

--- ClinicFlow/Views/LandingView.swift
+++ ClinicFlow/Views/LandingView.swift
@@ -7,61 +7,62 @@ struct LandingView: View {
     private let backgroundColor = Color(#colorLiteral(red: 0.1490196078, green: 0.3607843137, blue: 0.9960784314, alpha: 0.9672237169))
     
     var body: some View {
-        ZStack {
-            // Background
-            backgroundColor
-                .ignoresSafeArea()
+        
+        NavigationStack {
             
-            VStack(spacing: 0) {
-                Spacer()
+            ZStack {
                 
-                // Title & Subtitle
-                VStack(spacing: 10) {
-                    Text("Clinic Flow")
-                        .font(.system(size: 40, weight: .bold))
-                        .foregroundColor(.white)
+                // Background
+                backgroundColor
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 0) {
                     
-                    Text("Your clinic visit, organized\nand Simplified")
-                        .font(.system(size: 22, weight: .semibold))
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .lineSpacing(4)
-                }
-                
-                Spacer()
-                
-                // Get Started Button
-                Button(action: {
-                    viewModel.getStartedTapped()
-                }) {
-                    HStack(spacing: 12) {
-                        Image("ic_landing")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 36, height: 36)
+                    Spacer()
+                    
+                    // Title & Subtitle
+                    VStack(spacing: 10) {
+                        Text("Clinic Flow")
+                            .font(.system(size: 40, weight: .bold))
+                            .foregroundColor(.white)
                         
-                        Text("Get Started")
-                            .font(.system(size: 25, weight: .semibold))
-                            .foregroundColor(.black)
+                        Text("Your clinic visit, organized\nand Simplified")
+                            .font(.system(size: 22, weight: .semibold))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .lineSpacing(4)
                     }
-                    .padding(.horizontal, 22)
-                    .padding(.vertical, 14)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.white)
-                    .cornerRadius(50)
+                    
+                    Spacer()
+                    
+                    // Get Started Button
+                    NavigationLink(destination: InstructOneView()) {
+                        
+                        HStack(spacing: 12) {
+                            Image("ic_landing")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 36, height: 36)
+                            
+                            Text("Get Started")
+                                .font(.system(size: 25, weight: .semibold))
+                                .foregroundColor(.black)
+                        }
+                        .padding(.horizontal, 22)
+                        .padding(.vertical, 14)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(50)
+                    }
+                    .padding(.horizontal, 39)
+                    .padding(.bottom, 48)
                 }
-                .padding(.horizontal, 39)
-                .padding(.bottom, 48)
             }
-        }
-        .fullScreenCover(isPresented: $viewModel.isNavigating) {
-            //Have to replace with next screen
-            InstructOneView()
+            .navigationBarHidden(true)
         }
     }
 }
 
-// MARK: - Preview
 #Preview {
     LandingView()
 }
--- ClinicFlow/Views/SetupView.swift
+++ ClinicFlow/Views/SetupView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SetupView: View {
     var body: some View {
         Text("This is setup page")
+        Text("Where patients are going to be logged in")
     }
 }
 
