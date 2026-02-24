44fc09d394f1688ed66983ff95f476ffef5ad0b7
Thirandidesilva <thirandidesilva2002@gmail.com>
2026-02-24 09:48:26 +0530

    Created Main Landing Page inside the View folder

---
+++ ClinicFlow/Views/Main Landing Page.swift
@@ -0,0 +1,19 @@
+//
+//  Main Landing Page.swift
+//  ClinicFlow
+//
+//  Created by Thirandi De Silva on 2026-02-24.
+//
+
+import SwiftUI
+
+struct Main_Landing_Page: View {
+    var body: some View {
+        Text("Clinic Folw")
+        Text("Testing github")
+    }
+}
+
+#Preview {
+    Main_Landing_Page()
+}
